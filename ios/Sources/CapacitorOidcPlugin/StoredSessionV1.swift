52937425fd623be0706d37136c64723c7c3b67f7
Jojo Pirker <19796430+jojopirker@users.noreply.github.com>
2026-08-24 18:11:31 +0200

    feat: add iOS authentication adapter

---
+++ ios/Sources/CapacitorOidcPlugin/StoredSessionV1.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+public struct StoredSessionV1: Codable, Equatable, Sendable {
+    public let version: Int
+    public let issuer: String
+    public let clientId: String
+    public let accessToken: String
+    public let refreshToken: String?
+    public let idToken: String?
+    public let tokenType: String
+    public let scope: String?
+    public let expiresAt: Int?
+
+    public init(
+        version: Int = 1,
+        issuer: String,
+        clientId: String,
+        accessToken: String,
+        refreshToken: String? = nil,
+        idToken: String? = nil,
+        tokenType: String,
+        scope: String? = nil,
+        expiresAt: Int? = nil
+    ) {
+        self.version = version
+        self.issuer = issuer
+        self.clientId = clientId
+        self.accessToken = accessToken
+        self.refreshToken = refreshToken
+        self.idToken = idToken
+        self.tokenType = tokenType
+        self.scope = scope
+        self.expiresAt = expiresAt
+    }
+}
